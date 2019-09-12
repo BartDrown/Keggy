cfdc55171c5953efa2d97c1ba6f630be8ad0ce91
Unknown <kawaxbart@gmail.com>
2019-09-13 00:27:48 +0200

    Add basic movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] int speed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        rb.velocity = new Vector2(horizontal, vertical);
+        //transform.position += new Vector3(horizontal, vertical);
+    }
+}
