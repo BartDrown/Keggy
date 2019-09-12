51ca370fee0919081a5dff34bf42e7583206f204
Bartosz Jakubowski <bjakubowski.dev@gmail.com>
2019-09-13 00:51:28 +0200

    Exchanged joints component in Rope
    That finally wokrs guys, wasn't that this what we waited for so long? Wasn't?

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -17,28 +17,18 @@ public class Rope : MonoBehaviour
         if (beginHandle == null)
             beginHandle = current;
 
-        current.GetComponent<SpringJoint2D>().connectedBody = beginHandle.GetComponent<Rigidbody2D>();
+        current.GetComponent<DistanceJoint2D>().connectedBody = beginHandle.GetComponent<Rigidbody2D>();
 
         for(int i = 0; i < lenght; i++) {
             GameObject previous = current;
             current = Instantiate(prefab, transform);
-            current.GetComponent<SpringJoint2D>().connectedBody = previous.GetComponent<Rigidbody2D>();
+            current.GetComponent<DistanceJoint2D>().connectedBody = previous.GetComponent<Rigidbody2D>();
         }
 
         if (endHandle == null)
             endHandle = current;
 
-
-        
-        current.AddComponent<SpringJoint2D>().connectedBody = endHandle.GetComponent<Rigidbody2D>();
-        current.GetComponents<SpringJoint2D>()[1].distance = current.GetComponents<SpringJoint2D>()[0].distance;
-        current.GetComponents<SpringJoint2D>()[1].autoConfigureDistance = current.GetComponents<SpringJoint2D>()[0].autoConfigureDistance;
-        current.GetComponents<SpringJoint2D>()[1].frequency = current.GetComponents<SpringJoint2D>()[0].frequency;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        current.AddComponent<DistanceJoint2D>().connectedBody = endHandle.GetComponent<Rigidbody2D>();
+        current.GetComponents<DistanceJoint2D>()[1].distance = current.GetComponents<DistanceJoint2D>()[0].distance;
     }
 }
