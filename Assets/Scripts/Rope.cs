9e9885a8a2266410156a5d12414b4f0ec05844e9
unknown <bjakubowski.dev@gmail.com>
2019-09-12 16:35:07 +0200

    Added Rope Generator
    Don't try to rely on this rope if your life depends on it because its kinda springy

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    List<GameObject> ropeContainer;
+
+    [SerializeField] GameObject prefab, beginHandle, endHandle;
+    [SerializeField] int lenght;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject current = Instantiate(prefab, transform);
+
+        if (beginHandle == null)
+            beginHandle = current;
+
+        current.GetComponent<DistanceJoint2D>().connectedBody = beginHandle.GetComponent<Rigidbody2D>();
+
+        for(int i = 0; i < lenght; i++) {
+            GameObject previous = current;
+            current = Instantiate(prefab, transform);
+            current.GetComponent<DistanceJoint2D>().connectedBody = previous.GetComponent<Rigidbody2D>();
+        }
+
+        if (endHandle == null)
+            endHandle = current;
+        current.AddComponent<DistanceJoint2D>().connectedBody = endHandle.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
