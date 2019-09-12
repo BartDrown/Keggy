62485f9014e681f6f8f4ad134eb792464d80cf25
unknown <bjakubowski.dev@gmail.com>
2019-09-12 18:18:06 +0200

    Added Spring
    Don't let yourself carried on too much cowboy because it can draw you back

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -17,17 +17,23 @@ public class Rope : MonoBehaviour
         if (beginHandle == null)
             beginHandle = current;
 
-        current.GetComponent<DistanceJoint2D>().connectedBody = beginHandle.GetComponent<Rigidbody2D>();
+        current.GetComponent<SpringJoint2D>().connectedBody = beginHandle.GetComponent<Rigidbody2D>();
 
         for(int i = 0; i < lenght; i++) {
             GameObject previous = current;
             current = Instantiate(prefab, transform);
-            current.GetComponent<DistanceJoint2D>().connectedBody = previous.GetComponent<Rigidbody2D>();
+            current.GetComponent<SpringJoint2D>().connectedBody = previous.GetComponent<Rigidbody2D>();
         }
 
         if (endHandle == null)
             endHandle = current;
-        current.AddComponent<DistanceJoint2D>().connectedBody = endHandle.GetComponent<Rigidbody2D>();
+
+
+        
+        current.AddComponent<SpringJoint2D>().connectedBody = endHandle.GetComponent<Rigidbody2D>();
+        current.GetComponents<SpringJoint2D>()[1].distance = current.GetComponents<SpringJoint2D>()[0].distance;
+        current.GetComponents<SpringJoint2D>()[1].autoConfigureDistance = current.GetComponents<SpringJoint2D>()[0].autoConfigureDistance;
+        current.GetComponents<SpringJoint2D>()[1].frequency = current.GetComponents<SpringJoint2D>()[0].frequency;
     }
 
     // Update is called once per frame
