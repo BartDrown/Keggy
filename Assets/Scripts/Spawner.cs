2301baa2c78fa5b61abfbd94381cc8a62f3b6e60
Bartosz Jakubowski <bjakubowski.dev@gmail.com>
2019-09-13 15:52:18 +0200

    Added spawner
    Awww man, a creeper

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject barrel;
+    [SerializeField]
+    float spawnTime = 3f;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnBarrel", spawnTime, spawnTime);
+    }
+
+    void Update()
+    {
+    }
+    void SpawnBarrel()
+    {
+        GameObject.Instantiate(barrel, transform.position, transform.rotation, gameObject.transform);
+    }
+}
