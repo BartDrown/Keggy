46fda69b9a9f249ae81a7db6be5a0b7e85d629bb
Unknown <kawaxbart@gmail.com>
2019-09-14 00:39:02 +0200

    Add checkpoint minigame

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    Sprite checkSprite, uncheckedSprite;
+    public bool IsChecked { get; private set; }
+
+    //private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        IsChecked = true;
+        GetComponent<SpriteRenderer>().sprite = checkSprite;
+    }
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IsChecked = false;
+        GetComponent<SpriteRenderer>().sprite = uncheckedSprite;
+    }
+}
---
+++ Assets/Scripts/Endpoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Endpoint : MonoBehaviour
+{
+    [SerializeField] Checkpoint[] checkpoints;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name != "Keggy")
+            return;
+
+        bool isWon = true;
+
+        foreach (var checkpoint in checkpoints)
+        {
+            if (!checkpoint.IsChecked)
+                isWon = false;
+        }
+
+        if (isWon)
+            Debug.LogWarning("Won!");
+    }
+}
